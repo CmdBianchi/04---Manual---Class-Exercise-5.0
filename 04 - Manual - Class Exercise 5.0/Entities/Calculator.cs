912d908ca760c0f6b7becbc11508108cfd3cd1c4
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-26 15:27:43 -0300

    Commit 02

--- 04 - Manual - Class Exercise 5.0/Entities/Calculator.cs
+++ 04 - Manual - Class Exercise 5.0/Entities/Calculator.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 namespace Entities {
     class Calculator {
-
+        public double Pi = Math.PI;
+        public double Circumference(double r) {
+            return 2.0 * Pi * r;
+        }
+        public double Volume(double r) {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
     }
 }
--- 04 - Manual - Class Exercise 5.0/Program.cs
+++ 04 - Manual - Class Exercise 5.0/Program.cs
@@ -2,21 +2,16 @@
 using Entities;
 namespace _04___Manual___Class_Exercise_5._0 {
     class Program {
-        static double Pi = Math.PI;        
+        
         static void Main(string[] args) {
+            Calculator calculator = new Calculator();
             Console.Write("Entre o valor do raio: "); double radius = double.Parse(Console.ReadLine());
-            double circ = Circumference(radius);
-            double volume = Volume(radius);
-            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
-            Console.WriteLine("Volume: " + volume.ToString("F2"));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F6"));
-        }
-        static double Circumference(double r) {
-            return 2.0 * Pi * r;
-        }
-        static double Volume(double r) {
-            return 4.0 / 3.0 * Pi * r * r * r;
-        }
+            double circ = calculator.Circumference(radius);
+            double vol = calculator.Volume(radius);
 
+            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
+            Console.WriteLine("Volume: " + vol.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + calculator.Pi.ToString("F6"));
+        }
     }
 }
