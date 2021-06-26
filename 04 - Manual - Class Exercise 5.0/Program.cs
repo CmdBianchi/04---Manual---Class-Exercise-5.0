f031bb7a361a695c50ef36b49e8e2a52a335e1f3
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-26 15:09:36 -0300

    Adicionar arquivos de projeto.

---
+++ 04 - Manual - Class Exercise 5.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Entities;
+namespace _04___Manual___Class_Exercise_5._0 {
+    class Program {
+        static double Pi = Math.PI;        
+        static void Main(string[] args) {
+            Console.Write("Entre o valor do raio: "); double radius = double.Parse(Console.ReadLine());
+            double circ = Circumference(radius);
+            double volume = Volume(radius);
+            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F6"));
+        }
+        static double Circumference(double r) {
+            return 2.0 * Pi * r;
+        }
+        static double Volume(double r) {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
